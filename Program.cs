ffe4873284637c919718e3871683b7c3ba5cb264
Sotheareth Ham <david.va@ezpizee.com>
2020-07-29 23:00:29 +0700

    change main

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace WebApplication1
 {
     public class Program
     {
+        //test main
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
